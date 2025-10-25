87d9f8aa2320d2e4e23bec6c2c8973bfcbcb94b1
Amdjed117 <127938067+Amdjed117@users.noreply.github.com>
2025-10-25 19:34:13 +0100

    Update MongoDB URL placeholder in config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,3 +1,3 @@
 
 export const PORT = 5555 ; 
-export const mongoDBURL = 'mongodb+srv://reflectionss404:9182amdjed@backenddb.lph9v.mongodb.net/books?retryWrites=true&w=majority&appName=backenddb'
+export const mongoDBURL = 'PROVIDE YOUR MONGODB URI HERE'
